c2329296221d167d0a73e6961f834f54d26e422f
GoldenSource <50501111+Golden-Source@users.noreply.github.com>
2022-05-27 01:26:32 +0430

    Create sample.js

---
+++ sample.js
@@ -0,0 +1,15 @@
+const data = null;
+
+const xhr = new XMLHttpRequest();
+xhr.withCredentials = true;
+
+xhr.addEventListener("readystatechange", function () {
+  if (this.readyState === this.DONE) {
+    console.log(this.responseText);
+  }
+});
+
+xhr.open("GET", "https://verify-api.goldensource.ir/information");
+xhr.setRequestHeader("Authorization", "Bearer test");
+
+xhr.send(data);
